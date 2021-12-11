68a41c4b31ab7a9dd4731eddb58774305fee97b8
merumerutho <merutochan@gmail.com>
2021-12-11 13:53:13 +0100

    Adding "test_publisher" to communicate through UDP

---
+++ test_publisher/main.lua
@@ -0,0 +1,25 @@
+--lick = require "lick"
+--lick.reset = true
+
+-- NETWORK SETTINGS
+local socket = require "socket"
+local address, port = "127.0.0.1", 55555
+start = love.timer.getTime()
+i=0
+love.window.setVSync(false)
+function love.load()
+	udp = socket.udp()
+	udp:settimeout(0.001)
+	udp:setpeername(address, port)
+	--udp:send("test")
+end
+
+function love.draw()
+	love.graphics.print(i)
+end
+
+
+function love.update()
+	i=i+1
+	udp:send("Success!")
+end
