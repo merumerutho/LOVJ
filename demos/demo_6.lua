8602e8e557472718fea4839b13457e55733f3f75
merumerutho <merutochan@gmail.com>
2022-08-28 16:36:26 +0200

    refactoring of demo6, added movement

--- demos/demo_6.lua
+++ demos/demo_6.lua
@@ -3,31 +3,38 @@ kp = require "lib/utils/keypress"
 
 -- import pico8 palette
 local PALETTE
+local hang
+local BG_SPRITE_SIZE = 8
 
 patch = {}
 
+--- @private get_bg get background graphics based on resources
 local function get_bg()
 	patch.graphics = {}
 	patch.graphics.bg = {}
 	patch.graphics.bg.image = love.graphics.newImage(g:get("bg"))
-	patch.graphics.bg.size = {x = 8, y = 8}
+	patch.graphics.bg.size = {x = BG_SPRITE_SIZE, y = BG_SPRITE_SIZE}
 	patch.graphics.bg.frames = {}
-	for i=0,3 do
-		table.insert(patch.graphics.bg.frames, love.graphics.newQuad(i*8, 0, 8, 8, patch.graphics.bg.image))
+	for i=0,patch.graphics.bg.image:getWidth() / BG_SPRITE_SIZE do
+		table.insert(patch.graphics.bg.frames, love.graphics.newQuad(	i*BG_SPRITE_SIZE,			-- x
+																		0,							-- y
+																		BG_SPRITE_SIZE,				-- width
+																		BG_SPRITE_SIZE,				-- height
+																		patch.graphics.bg.image))	-- img
 	end
 end
 
-
+--- @private init_params initialize patch parameters
 local function init_params()
 	g = resources.graphics
 	p = resources.parameters
 	g:setName(1, "bg")				g:set("bg", "data/demo_6/bg.png")
 	get_bg()
-	p:setName(1, "bgSpeed")			p:set("bgSpeed", 3)
+	p:setName(1, "bgSpeed")			p:set("bgSpeed", 10)
 end
 
-
-function patch.patchControls()
+--- @private patchControls evaluate user keyboard controls
+local function patchControls()
 	p = resources.parameters
 	-- Hanger
 	if kp.isDown("x") then hang = true else hang = false end
@@ -45,15 +52,21 @@ function patch.init()
 	init_params()
 end
 
-
+--- @private draw_bg draw background graphics
 local function draw_bg()
 	g = resources.graphics
 	p = resources.parameters
 
-	local idx = (math.floor(timer.T * p:get("bgSpeed") ) % (patch.graphics.bg.image:getWidth() / 8) ) + 1
-	for x = 0, screen.InternalRes.W, patch.graphics.bg.size.x do
-		for y = 0, screen.InternalRes.H, patch.graphics.bg.size.y do
-			love.graphics.draw(patch.graphics.bg.image, patch.graphics.bg.frames[idx], x, y)
+	local idx = (math.floor(timer.T * p:get("bgSpeed") ) % (patch.graphics.bg.image:getWidth() / BG_SPRITE_SIZE) ) + 1
+	for x = -patch.graphics.bg.size.x, screen.InternalRes.W, patch.graphics.bg.size.x do
+		for y = -patch.graphics.bg.size.y, screen.InternalRes.H, patch.graphics.bg.size.y do
+			local lx = x + (timer.T*20)% BG_SPRITE_SIZE
+			local ly = y + (timer.T*10)% BG_SPRITE_SIZE
+			local rx = (lx - screen.InternalRes.W / 2)
+			local ry = (ly - screen.InternalRes.H / 2)
+			local rIdx = math.floor((idx + math.sqrt((rx*rx) + (ry*ry)) / 10)
+									% (patch.graphics.bg.image:getWidth() / BG_SPRITE_SIZE )) + 1
+			love.graphics.draw(patch.graphics.bg.image, patch.graphics.bg.frames[rIdx], lx, ly)
 		end
 	end
 
@@ -89,7 +102,7 @@ end
 
 function patch.update()
 	-- apply keyboard patch controls
-	if not cmd.isOpen then patch.patchControls() end
+	if not cmd.isOpen then patchControls() end
 	return
 end
 
