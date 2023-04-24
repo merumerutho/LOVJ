24a95a1088595736ca96a8753144cceb7aca2531
merumerutho <merutochan@gmail.com>
2023-04-24 15:13:49 +0200

    Adding demo 11 to demonstrate loop points functionality.

---
+++ demos/demo_11.lua
@@ -0,0 +1,103 @@
+local Patch = lovjRequire("lib/patch")
+local palettes = lovjRequire("lib/utils/palettes")
+local videoutils = lovjRequire("lib/utils/video")
+local screen_settings = lovjRequire("lib/cfg/cfg_screen")
+local shaders = lovjRequire("lib/shaders")
+local Timer = lovjRequire("lib/timer")
+local cfg_timers = lovjRequire("lib/cfg/cfg_timers")
+
+
+-- import pico8 palette
+local PALETTE
+
+patch = Patch:new()
+
+--- @public setCanvases (re)set canvases for this patch
+function patch:setCanvases()
+	Patch.setCanvases(patch)  -- call parent function
+	-- patch-specific execution (video canvas)
+	if screen_settings.UPSCALE_MODE == screen_settings.LOW_RES then
+		patch.canvases.video = love.graphics.newCanvas(screen.InternalRes.W, screen.InternalRes.H)
+	else
+		patch.canvases.video = love.graphics.newCanvas(screen.ExternalRes.W, screen.ExternalRes.H)
+	end
+end
+
+--- @private init_params initialize patch parameters
+local function init_params()
+	g = resources.graphics
+	p = resources.parameters
+
+    g:setName(1, "video")           g:set("video", "data/demo_11/output.ogg")
+end
+
+--- @public patchControls evaluate user keyboard controls
+function patch.patchControls()
+	p = resources.parameters
+
+    -- insert here your patch controls
+end
+
+
+--- @public init init routine
+function patch.init()
+	PALETTE = palettes.PICO8
+
+	patch.setCanvases()
+
+	init_params()
+
+    patch.video = {}
+    patch.video.handle = love.graphics.newVideo(g:get("video"))
+    patch.video.pos = 0
+	patch.video.scaleX = screen.InternalRes.W / patch.video.handle:getWidth()
+	patch.video.scaleY = screen.InternalRes.H / patch.video.handle:getHeight()
+    patch.video.handle:play()
+
+	patch:assignDefaultDraw()
+end
+
+--- @private draw_bg draw background graphics
+local function draw_stuff()
+	g = resources.graphics
+	p = resources.parameters
+
+end
+
+--- @public patch.draw draw routine
+function patch.draw()
+	patch:drawSetup()
+
+	local t = cfg_timers.globalTimer.T
+
+	love.graphics.clear()
+
+	-- set canvas
+	love.graphics.setCanvas(patch.canvases.video)
+
+	-- render graphics
+	love.graphics.draw(patch.video.handle, 0, 0, 0, patch.video.scaleX, patch.video.scaleY)
+	-- set main canvas
+	love.graphics.setCanvas(patch.canvases.main)
+	-- draw video w/ chroma keying
+	if screen.isUpscalingHiRes() then
+		love.graphics.draw(patch.canvases.video, 0, 0, 0, screen.Scaling.X, screen.Scaling.Y)
+	else
+		love.graphics.draw(patch.canvases.video)
+	end
+	patch:drawExec()
+end
+
+
+function patch.update()
+    patch:mainUpdate()
+    -- handle loop
+    videoutils.handleLoop(patch.video, 0, 3.9)  -- loop between 0 and 6
+end
+
+
+function patch.commands(s)
+
+end
+
+return patch
--- lib/utils/video.lua
+++ lib/utils/video.lua
@@ -6,14 +6,18 @@
 local videoutils = {}
 
 --- @public handleLoop Upon vide play termination, it rewinds and starts the video again, creating a loop.
-function videoutils.handleLoop(vid)
-    -- loop video
-    if vid.handle:tell() >= vid.pos then
+function videoutils.handleLoop(vid, loop_start, loop_end)
+    -- without loop_point: try to reach end of video then restart
+    -- with loop point: reach loop point and then restart
+    loop_start = 0 or loop_start
+    -- ...
+    if (vid.handle:tell() >= vid.pos and not loop_end) or
+            (loop_end and vid.handle:tell() < loop_end) then
         vid.pos = vid.handle:tell()
     else
-        vid.handle:rewind()
+        vid.handle:seek(loop_start)
         vid.handle:play()
-        vid.pos = 0
+        vid.pos = loop_start
     end
 end
 
