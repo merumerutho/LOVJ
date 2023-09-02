a177da8333328ee6950d4cb8ca3d3c03e02951df
merumerutho <merutochan@gmail.com>
2023-09-02 08:35:43 +0200

    Improved swirl shader

--- lib/shaders.lua
+++ lib/shaders.lua
@@ -70,6 +70,13 @@ shaders.circleswirl = [[
         vec2 distortedUV = center + 0.707*vec2(cos(angle + swirlAngle+uv.x*_swirlmodx),
                                                sin(angle + swirlAngle+uv.y*_swirlmody)) * mod(sin(_time+uv.x*uv.y), distance*2);
 
+        distortedUV = distortedUV + 0.707 * vec2(cos(angle + swirlAngle + _time*.8), sin(angle + swirlAngle)) * distance;
+
+        distortedUV.y = ((distortedUV.x-.1) * (distortedUV.y + .1)) + ((distortedUV.y-.1) * (distortedUV.x + .1));
+
+        distortedUV.x += sin(distortedUV.x);
+        distortedUV.x = mod(distortedUV.x, 1);
+
         color = vec4(Texel(tex, distortedUV));
         return color;
 	}
