4477a843eaa12381e37414f142bad2fdc1874ada
merumerutho <merutochan@gmail.com>
2024-08-08 18:30:22 +0200

    Added drawing utils.

---
+++ lib/utils/drawing.lua
@@ -0,0 +1,30 @@
+-- drawing.lua
+--
+-- Drawing utils
+
+local DrawingUtils = {}
+
+function DrawingUtils.drawCanvasToCanvas(src, dst, x, y, r, sx, sy)
+	-- set defaults
+	local x = x or 0
+	local y = y or 0
+	local r = r or 0
+	local sx = sx or 0
+	local sy = sy or 0
+	-- set canvas, draw and go back to previous canvas
+	local currentCanvas = love.graphics.getCanvas()
+	love.graphics.setCanvas(dst)
+	love.graphics.draw(src, x, y, r, sx, sy)
+	love.graphics.setCanvas(currentCanvas)
+	return dst
+end
+
+function DrawingUtils.clearCanvas(c)
+	local currentCanvas = love.graphics.getCanvas()
+	love.graphics.setCanvas(c)
+	love.graphics.clear()
+	love.graphics.setCanvas(currentCanvas)
+	return c
+end
+
+return DrawingUtils
