c524c43785cae93bc0b3bab8a86581941e78cf6c
merumerutho <merutochan@gmail.com>
2025-07-11 17:44:34 +0200

    Hex to RGB utility function

--- lib/utils/palettes.lua
+++ lib/utils/palettes.lua
@@ -53,4 +53,12 @@ function Palettes.getColor(p, idx)
 	return {p[idx][1] / 255, p[idx][2] / 255, p[idx][3] / 255}
 end
 
+function Palettes.hexToRgb(hex)
+    hex = hex:gsub("#", "")
+    local r = tonumber(hex:sub(1,2), 16) / 255
+    local g = tonumber(hex:sub(3,4), 16) / 255
+    local b = tonumber(hex:sub(5,6), 16) / 255
+    return r, g, b
+end
+
 return Palettes
