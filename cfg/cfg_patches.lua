62aacbc02b20271f3cfe95c9bb8d608ab0de7784
merumerutho <merutochan@gmail.com>
2025-07-18 18:45:43 +0200

    Moved assets to demos. Restructured accordingly.

--- cfg/cfg_patches.lua
+++ cfg/cfg_patches.lua
@@ -6,21 +6,21 @@
 local cfg_patches = {}
 
 --- @public DEFAULT_PATCH default patch loaded at startup
-cfg_patches.defaultPatch = {"demos/source/demo_24"}
+cfg_patches.defaultPatch = {"demos/demo24/source/demo_24"}
 
 --- @public patches list of patches
-cfg_patches.patches = {"demos/source/demo_20",
-                       "demos/source/demo_3",
-                       "demos/source/demo_4",
-                       "demos/source/demo_5",
-                       "demos/source/demo_6",
-                       "demos/source/demo_16",
-                       "demos/source/demo_8",
-                       "demos/source/demo_13",
-                       "demos/source/demo_18",
-                       "demos/source/demo_11",
-                       "demos/source/demo_12",
-                       "demos/source/demo_14"
+cfg_patches.patches = {"demos/demo20/source/demo_20",
+                       "demos/demo3/source/demo_3",
+                       "demos/demo4/source/demo_4",
+                       "demos/demo5/source/demo_5",
+                       "demos/demo6/source/demo_6",
+                       "demos/demo16/source/demo_16",
+                       "demos/demo8/source/demo_8",
+                       "demos/demo13/source/demo_13",
+                       "demos/demo18/source/demo_18",
+                       "demos/demo11/source/demo_11",
+                       "demos/demo12/source/demo_12",
+                       "demos/demo14/source/demo_14"
 }
 
 
